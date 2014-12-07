11583dd763b123d5d9f8fe2aaec59c79554e3d09
Dave Woodall <dave.woodall@livingsocial.com>
2014-12-06 21:50:29 -0700

    build

--- build/scripts/main-233162ab.js
+++
@@ -1,1 +0,0 @@
-(function(){$(function(){return $(".click-me").click(function(){return $("#laird").slideDown().addClass("BounceInDown"),$(".jetski-img").addClass("bounceInRight")}),$("#laird").click(function(){return $("#laird").slideUp()})})}).call(this);
